aa3099114810ee461d674204d383f879cd7fffcb
Joonspar <166091275+Joonspar@users.noreply.github.com>
2024-07-12 12:06:17 +0900

    Add files via upload

---
+++ 1929_소수 구하기.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+m, n = map(int, input().split())
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    if num == 2 or num == 3:
+        return True
+    if num % 2 == 0 or num % 3 == 0:
+        return False
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+for i in range(m, n + 1):
+    if is_prime(i):
+        print(i)
